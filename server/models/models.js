c2a9d51da7d9b5b7168b949c183f02fcd933b151
Viktor Ryabov <iprva@ya.ru>
2023-04-25 17:01:16 +0300

    started server-side

---
+++ server/models/models.js
@@ -0,0 +1,36 @@
+const sequelize = require('../db');
+const {DataTypes} = require('sequelize');
+
+const User = sequelize.define( 'user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING, unique: true},
+    role: {type: DataTypes.STRING, defaultValue: "GAMER"},    
+    currentQuestion: {type: DataTypes.INTEGER},
+});
+
+const Location = sequelize.define('location', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    description: {type: DataTypes.STRING, allowNull: false},
+    image: {type: DataTypes.STRING, allowNull: false},
+});
+
+const Question = sequelize.define('question', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    description: {type: DataTypes.STRING, allowNull: false},
+});
+
+User.hasMany(Location)
+Question.belongsTo(User);
+
+User.hasMany(Question)
+Question.belongsTo(User);
+
+Location.hasOne(Question);
+Question.belongsTo(Location);
+
+module.exports = {
+    User,
+    Location,
+    Question
+}
